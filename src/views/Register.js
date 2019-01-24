0f1c701be2b8f68b2d002ea94acf26b2a6a64c0d
Christopher Payne <cpayne36@gatech.edu>
2019-01-24 00:54:02 -0500

    Added ability to identify as an admin and a space to take in a valid admin code

--- src/views/Register.js
+++ src/views/Register.js
@@ -2,48 +2,59 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import NavButton from './NavButton'
 
-var firebase = require("firebase");
-
-export default class Register extends Component {
-  state = {
+export default class Register extends Component { 
+  state = { 
   }
 
   constructor(props) {
     super(props);
-    this.registerUser = this.registerUser.bind(this);
+    this.checkIt = this.checkIt.bind(this);
+    this.unCheckIt = this.unCheckIt.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      checked:false
+    };
   }
 
-  registerUser(e) {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      alert(errorCode + '\n' + errorMessage);
-      // ...
+  checkIt() {
+    this.setState({
+      checked:true
     });
-
-    e.preventDefault();
   }
 
-  render () {
+  unCheckIt() {
+    this.setState({
+      checked:false
+    });
+  }
+
+  handleChange(evt) {
+    if(this.state.checked !== evt.target.checked) {
+       this.setState({
+         checked:evt.target.checked
+       });
+    }
+ }
+  
+  render () {                                   
       return (
         <div>
              <div id='signinContainer'>
-              <form id='form' onSubmit={this.registerUser}>
-                <input className='input' type="text"
-                  placeholder="Email" id="email"/>
-                <input className='input' type="password"
-                  placeholder="Password" id="password"/>
-                <input className='input' type="password"
-                  placeholder="Confirm Password"/>
-                <button id='submit'>Sign Up</button>
+              <form id='form' onSubmit={this.onSubmit.bind(this)}>
+                <input className='input' name='email' type="text"
+                  placeholder="Email"/>
+                <input className='input' name='pass' type="password"
+                  placeholder="Password"/>          
+                <input className='input' name='cPass' type="password"  
+                  placeholder="Confirm Password"/>          
+                <button id='submit' type="submit">Sign Up</button>
               </form>
              </div>
-             <br></br>
+              <div>
+                I'm an Administrator: <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                  {this.state.checked ? <input className='input' name='email' type="text"
+                  placeholder="Admin Code" onChange={this.onClick}/> : null}
+              </div>
              <div>
               <NavButton to='/login'>Login</NavButton>
               <NavButton to='/dashboard'>Dashboard</NavButton>
@@ -51,4 +62,14 @@ export default class Register extends Component {
         </div>
       )
    }
+
+  onSubmit(e){
+      var user = {
+          email: e.target.elements.email.value,
+          pass: e.target.elements.pass.value,
+          id: Math.floor(Math.random() * 10000000000)
+      }
+      console.log(user)
+  }
+
 }
