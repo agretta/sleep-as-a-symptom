a0d2a15ccb786fbb142198b041b2d0c708ca7f77
Anna K. Klaussen <anna.klaussen@gmail.com>
2019-02-05 20:36:30 -0500

    add login form

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,28 +1,69 @@
 
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
 import NavButton from '../components/NavButton'
-export default class Login extends Component { 
-  state = { 
+import { Container, Row, Col, Button, FormControl, FormGroup, Form } from 'react-bootstrap'
+
+export default class Login extends Component {
+
+  state = {}
+
+  constructor(props) {
+    super(props)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.state = {
+      email:'',
+      pass:'',
+      to_dashboard:false
+    };
   }
-  
-  render () {                                   
-      return (
-        <div>
-            <div id='loginContainer'>
-                 <form id='form'>       
-                    <input className='input' type="text"   
-                    placeholder="Email"/>
-                    <input className='input' type="password"  
-                    placeholder="Password"/>          
-                    <button id='submit'>Register</button>
-                </form>
-            </div>
-            <div>
-              <NavButton to='/register'>Register</NavButton>
-              <NavButton to='/dashboard'>Dashboard</NavButton>
-            </div>
-        </div>
-      )
+
+
+  handleChangeEmail(evt) {
+     this.setState({
+       email: evt.target.value
+     });
+  }
+  handleChangePass(evt) {
+      this.setState({
+        pass: evt.target.value
+      });
    }
+
+
+   moveToDash() {
+     this.setState({ to_dashboard: true });
+   }
+
+
+  render () {
+        if (this.state.to_dashboard == true) {
+              return <Redirect to='/dashboard' />
+        }
+
+        return (
+          <Container>
+          <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Form id='form' onSubmit={this.registerUser}>
+                <FormGroup>
+                  <Row style={{display: 'flex', justifyContent: 'center',}}>
+                    <FormControl className='input' name='email' type="text" id="email"
+                      placeholder="Email" value={this.state.email} onChange={this.handleChangeEmail}/>
+                  </Row>
+                  <Row style={{display: 'flex', justifyContent: 'center',}}>
+                    <FormControl className='input' name='pass' type="password" id="password"
+                      placeholder="Password" value={this.state.pass} onChange={this.handleChangePass}/>
+                  </Row>
+
+                <Row style={{display: 'flex', justifyContent: 'center',}}>
+                  <Button variant='outline-primary' id='submit' type="submit">Login</Button>
+                  </Row>
+                </FormGroup>
+                </Form>
+          </Col>
+          </Container>
+        )
+     }
+
 }
