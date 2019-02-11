10c911fd6575d0fab1229384c7af4cd5ce54e0d5
graham-ely <firstclass140@gmail.com>
2019-02-11 07:50:52 -0500

    Add FB connect function

--- src/views/FBSettings.js
+++ src/views/FBSettings.js
@@ -4,45 +4,55 @@ import NavButton from '../components/NavButton'
 import { Container, Row, Col, Button, FormControl, FormGroup, Form } from 'react-bootstrap'
 
 var firebase = require("firebase");
+
 export default class FBSettings extends Component {
-   state = {
-   }
+  state = {
+  }
 
-   constructor(props) {
-     super(props);
-     this.handleChangeEmail = this.handleChangeEmail.bind(this);
-     this.handleChangePass = this.handleChangePass.bind(this);
-     this.handleChangecPass = this.handleChangecPass.bind(this);
-     this.state = {
-       fb_email:'',
-       fb_pass:'',
-       fb_auth:false
-     };
+  constructor(props) {
+    super(props);
+    this.setUserAuthtoken = this.setUserAuthtoken.bind(this);
+    this.deleteUserAuthtoken = this.deleteUserAuthtoken.bind(this);
+    this.state = {
+      fb_auth:false
+    };
 
-   }
+  }
 
-    handleChange(evt) {
-    if(this.state.checked !== evt.target.checked) {
-        this.setState({
-            checked:evt.target.checked
-        });
-    }
-    }
-    handleChangeEmail(evt) {
-    this.setState({
-        email: evt.target.value
+  setUserAuthtoken(e){
+
+    var user = firebase.auth().currentUser.uid;
+
+    // grab & parse the user's access token for the FB API
+    var token_regex = /access_token=.*(?=&user_id)/g;
+    var token       = token_regex.exec( window.location.href )[0].substring(13);
+
+    // set the token in the DB
+    firebase.database().ref( 'users/' + user ).set({
+        api_token : token,
     });
-    }
-    handleChangePass(evt) {
-        this.setState({
-        pass: evt.target.value
-        });
-    }
-    handleChangecPass(evt) {
-        this.setState({
-        cPass: evt.target.value
-        });
-    }
+
+    this.setState({
+        fb_auth : true,
+    });
+
+  }
+
+  deleteUserAuthtoken(e){
+
+    // grab the user's access token for the FB API
+    var user        = firebase.auth().currentUser.uid;
+
+    firebase.database().ref( 'users/' + user ).set({
+        api_token : null,
+    });
+
+    this.setState({
+        fb_auth : false,
+    });
+  }
+
+  //TODO: implement data deletion once we implement data import
 
      render () {
      // https://react-bootstrap.github.io/components/forms/
@@ -50,13 +60,21 @@ export default class FBSettings extends Component {
              return (
                 <Container>
                 <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                      <Form id='form' onSubmit={this.registerUser}>
+                      <Form id='form_unlink_fitbit' onSubmit={this.deleteUserAuthtoken}>
                       <FormGroup>
                          <Row style={{display: 'flex', justifyContent: 'center',}}>
                             <Button variant='outline-primary' id='submit' type="submit">Unlink FitBit</Button>
                          </Row>
+                      </FormGroup>
+                      </Form>
+                </Col>
+                <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                      <Form id='form_delete_data' onSubmit={this}>
+                      <FormGroup>
                          <Row style={{display: 'flex', justifyContent: 'center',}}>
                             <Button variant='outline-primary' id='submit' type="submit">Delete Data</Button>
+                         </Row>
+                         <Row style={{display: 'flex', justifyContent: 'center',}}>
                          <NavButton to='/dashboard'>Cancel</NavButton>
                          </Row>
                       </FormGroup>
@@ -71,25 +89,18 @@ export default class FBSettings extends Component {
           return (
              <Container>
              <Col style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                   <Form id='form' onSubmit={this.registerUser}>
+                   <Form id='form' onSubmit={this.setUserAuthtoken}>
                    <FormGroup>
                       <Row style={{display: 'flex', justifyContent: 'center',}}>
                       Link FitBit Account
                       </Row>
                       <Row style={{display: 'flex', justifyContent: 'center',}}>
-                         <FormControl className='input' name='email' type="text" id="email"
-                         placeholder="Email" value={this.state.email} onChange={this.handleChangeEmail}/>
+                      <Button variant='outline-secondary' href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DBZ7&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ffitbit-settings&scope=sleep&expires_in=31536000" role="button">Link FB</Button>
                       </Row>
                       <Row style={{display: 'flex', justifyContent: 'center',}}>
-                         <FormControl className='input' name='pass' type="password" id="password"
-                         placeholder="Password" value={this.state.pass} onChange={this.handleChangePass}/>
+                      <Button variant='outline-primary' id='submit' type="submit">Activate Link</Button>
                       </Row>
                       <Row style={{display: 'flex', justifyContent: 'center',}}>
-                      <FormControl className='input' name='cPass' type="password" id="confirm-password"
-                         placeholder="Confirm Password" value={this.state.cPass} onChange={this.handleChangecPass}/>
-                      </Row>
-                      <Row>
-                      <Button variant='outline-primary' id='submit' type="submit">Link FitBit</Button>
                       <NavButton to='/dashboard'>Cancel</NavButton>
                       </Row>
                    </FormGroup>
