a5a4a80caba86bf80a6a60a0d28a1dbdd9dddf0d
Sc0urg3r <Armad_R@gatech.edu>
2019-02-15 11:09:12 -0500

    Add button to refresh verification page #46

--- src/views/VerifyEmail.js
+++ src/views/VerifyEmail.js
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
 import {Redirect, Link} from 'react-router-dom';
 import NavButton from '../components/NavButton'
-import Header from '../components/Header'
 import { Container, Row, Col, Button, FormControl, FormGroup, Form } from 'react-bootstrap'
 
 var firebase = require("firebase");
 
 export default class Dashboard extends Component {
   state = {
+    to_dashboard: false
+  }
+
+  constructor(props) {
+    super(props);
+    this.authListener = this.authListener.bind(this);
+    this.refresh = this.refresh.bind(this);
+  }
+
+  componentWillMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    this.fireBaseListener = firebase.auth().onAuthStateChanged((user) => {
+        if (user.emailVerified) {
+          this.setState({
+            to_dashboard: true
+          });
+          console.log('verified');
+        } else {
+          console.log('unverified');
+        }
+      });
+    }
+
+  refresh() {
+    window.location.reload();
   }
 
   render () {
-      return (
-        <div>
-            <Header title='Verify Email'></Header>
-            <Row style={{display: 'flex', justifyContent: 'center',}}>
-            Please Verify Your Email To Continue
-            </Row>
-        </div>
-      )
+    if (this.state.to_dashboard == true) {
+      return <Redirect to='/dashboard' />
+    }
+    return (
+      <div>
+          <Row style={{display: 'flex', justifyContent: 'center'}}>
+          Please Verify Your Email To Continue
+          </Row>
+          <Row style={{display: 'flex', justifyContent: 'center'}}>
+            <Button variant='outline-primary' type="submit" onClick={this.refresh}>Complete</Button>
+          </Row>
+      </div>
+    )
    }
+
+  componentWillUnmount() {
+   this.fireBaseListener && this.fireBaseListener();
+   this.authListener = undefined;
+  }
 }
