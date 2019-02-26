2b9a13f3e1bf5cc0ca2b08bf5e7b51bb96078cba
graham-ely <graham.ely@gatech.edu>
2019-02-26 15:45:45 -0500

    Check for fitbit link on Sleep Viewer page #51

--- src/views/SleepViewer.js
+++ src/views/SleepViewer.js
@@ -2,17 +2,61 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import NavButton from '../components/NavButton'
 import Header from '../components/Header'
+
+var firebase = require("firebase");
+
 export default class SleepViewer extends Component { 
-  state = { 
+  state = {
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fb_auth : false,
+      title   : 'FitBit Settings',
+    };
   }
   
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        var uid = firebase.auth().currentUser.uid;
+        var ref = firebase.database().ref( "users/" + uid );
+
+        ref.once("value").then((snapshot) => {
+            var token_exists = snapshot.child("api_token").exists();
+
+            this.setState({
+              fb_auth : token_exists,
+            });
+          });
+      } else {
+        // No user is signed in,
+        // so we don't do anything.
+      }
+    });
+  }
+
   render () {                                   
+    if (this.state.fb_auth) {
+      return (
+        <div>
+          <Header title='Sleep Viewer: Logged In'></Header>
+             <br></br>
+	       <NavButton to='/dashboard'>Dashboard</NavButton>
+             </div>
+      )
+    } else {
       return (
         <div>
           <Header title='Sleep Viewer'></Header>
             <br></br>
-            <NavButton to='/dashboard'>Dashboard</NavButton>
+	    <p style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              Please link your FitBit to see your sleep data!
+            </p>
+          <NavButton to='/dashboard'>Dashboard</NavButton>
         </div>
       )
-   }
+    }
+  }
 }
